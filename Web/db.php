04be539dc8becd8cff05659f2856cceae4c46595
guilhermitoo <gh.dias1995@gmail.com>
2015-09-27 20:51:58 -0300

    algumas coisas banco e sistema web

---
+++ Web/db.php
@@ -0,0 +1,5 @@
+<?php
+
+// verifica se a conexão está aberta, se não estiver, abre a conexão
+
+?>
---
+++ Web/index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<html>
+    <script src="js/jquery-1.10.2.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+	<title>Controle de Chamados</title>
+	<body>
+		
+	</body>
+	<script src="js/bootstrap.min.js"></script>
+</html>
