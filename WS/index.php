57fa84dbbefeca439d43563b3638af3bc6a13f6c
guilhermitoo <gh.dias1995@gmail.com>
2015-11-12 20:57:58 -0200

    criadas as funções do web service

--- WS/index.php
+++ WS/index.php
@@ -1,9 +1,9 @@
 <?php
  
-require 'Slim/Slim.php';
+require 'vendor/autoload.php';
 require 'db.php';
  
-$app = new Slim();
+$app = new Slim\App();
  
 $app->get('/locais', 'getLocais');
 $app->get('/locais/:id', 'getLocal');
@@ -12,15 +12,13 @@ $app->get('/equipamentos/:id', 'getEquipamento');
 $app->get('/usuarios/','getUsuarios');
 $app->get('/usuarios/:id','getUsuario');
 $app->post('/usuarios/:nome/:email/:senha','cadUsuario');
-$app->put('/usuarios/:nome/:email/:senha','updateUsuario');
-$app->put('/chamados/:desc/:local_id/:equip_id','cadChamado');
+$app->put('/usuarios/:nome/:email/:senha','updateUsuario'); 
+$app->post('/chamados/:desc/:equip_id/:usuario_id','cadChamado');
 $app->get('/chamados','getChamados');
 $app->get('/chamados/:id','getChamado');
 
 $app->run();
- 
-$app->run();
- 
+
 function getLocais(){
     try{
         $query = $database->select('locais',['id','nome'],['ORDER'=>'nome']);
@@ -40,138 +38,94 @@ function getLocal($id){
 }
 
 function getEquipamentos(){
+    try{
+        $query = $database->select('equipamentos',['id','descricao','local_id'],['ORDER'=>'descricao']);
+        echo json_encode($query);
+    }catch(PDOException $e){
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }   
 }
 
 function getEquipamento($id){
+    try{
+        $query = $database->select('equipamentos',['id','descricao','local_id'],['id'=>$id]);
+        echo json_encode($query);
+    }catch(PDOException $e){
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
 }
 
 function getUsuarios(){
+    try{
+        $query = $database->select('usuarios',['id','nome','email','senha'],['ORDER'=>'nome']);
+        echo json_encode($query);
+    }catch(PDOException $e){
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }       
 }
 
 function getUsuario($id){
+    try{
+        $query = $database->select('usuarios',['id','nome','email','senha'],['id'=>$id]);
+        echo json_encode($query);
+    }catch(PDOException $e){
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }       
 }
 
 function cadUsuario($nome,$email,$senha){
+    try{
+        $data = date('y-m-d');
+        $qtd = $database->insert('usuarios',
+                                 ['nome'=>$nome,
+                                  'email'=>$email,
+                                  'senha'=>$senha]);
+        echo '{"qtd":'+$qtd+'}';
+    }catch(PDOException $e){
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }       
 }
 
 function updateUsuario($nome,$email,$senha){
+    try{
+        $qtd = $database->update('usuarios',['nome'=>$nome,'email'=>$email,'senha'=>md5($senha)],['email'=>$email]);
+        echo '{"qtd":'+$qtd+'}';
+    }catch(PDOException $e){
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }       
 }
 
-function cadChamado($desc,$local_id,$equip_id){
+function cadChamado($desc,$equip_id,$usuario_id){
+    try{
+        $data = date('y-m-d');
+        $qtd = $database->insert('chamados',
+                                 ['descricao'=>$descricao,
+                                  'data_inicio'=>$data,
+                                  'status'=>1,
+                                  'usuario_id'=>$usuario_id,
+                                  'equipamento_id'=>$equip_id]);
+        echo '{"qtd":'+$qtd+'}';
+    }catch(PDOException $e){
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }       
 }
 
+function getChamados(){
+    try{
+        $query = $database->select=('chamados'['id','descricao','data_inicio','data_fim','status','usuario_id','funcionario_id','equipamento_id',],['ORDER'=>'data_inicio']);
+        echo json_encode($query);
+    }catch(PDOException $e){
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
 
-
-function getWines() {
-    $sql = "select * FROM wine ORDER BY name";
-    try {
-        $db = getConnection();
-        $stmt = $db->query($sql);
-        $wines = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $db = null;
-        echo '{"wine": ' . json_encode($wines) . '}';
-    } catch(PDOException $e) {
+function getChamado($id){
+    try{
+        $query = $database->select=('chamados'['id','descricao','data_inicio','data_fim','status','usuario_id','funcionario_id','equipamento_id',],['id'=>$id]);
+        echo json_encode($query);
+    }catch(PDOException $e){
         echo '{"error":{"text":'. $e->getMessage() .'}}';
     }
-}
- 
-function getWine($id) {
-    $sql = "SELECT * FROM wine WHERE id=:id";
-    try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("id", $id);
-        $stmt->execute();
-        $wine = $stmt->fetchObject();
-        $db = null;
-        echo json_encode($wine);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
- 
-function addWine() {
-    $request = Slim::getInstance()->request();
-    $wine = json_decode($request->getBody());
-    $sql = "INSERT INTO wine (name, grapes, country, region, year, description) VALUES (:name, :grapes, :country, :region, :year, :description)";
-    try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("name", $wine->name);
-        $stmt->bindParam("grapes", $wine->grapes);
-        $stmt->bindParam("country", $wine->country);
-        $stmt->bindParam("region", $wine->region);
-        $stmt->bindParam("year", $wine->year);
-        $stmt->bindParam("description", $wine->description);
-        $stmt->execute();
-        $wine->id = $db->lastInsertId();
-        $db = null;
-        echo json_encode($wine);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
- 
-function updateWine($id) {
-    $request = Slim::getInstance()->request();
-    $body = $request->getBody();
-    $wine = json_decode($body);
-    $sql = "UPDATE wine SET name=:name, grapes=:grapes, country=:country, region=:region, year=:year, description=:description WHERE id=:id";
-    try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("name", $wine->name);
-        $stmt->bindParam("grapes", $wine->grapes);
-        $stmt->bindParam("country", $wine->country);
-        $stmt->bindParam("region", $wine->region);
-        $stmt->bindParam("year", $wine->year);
-        $stmt->bindParam("description", $wine->description);
-        $stmt->bindParam("id", $id);
-        $stmt->execute();
-        $db = null;
-        echo json_encode($wine);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
- 
-function deleteWine($id) {
-    $sql = "DELETE FROM wine WHERE id=:id";
-    try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("id", $id);
-        $stmt->execute();
-        $db = null;
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
- 
-function findByName($query) {
-    $sql = "SELECT * FROM wine WHERE UPPER(name) LIKE :query ORDER BY name";
-    try {
-        $db = getConnection();
-        $stmt = $db->prepare($sql);
-        $query = "%".$query."%";
-        $stmt->bindParam("query", $query);
-        $stmt->execute();
-        $wines = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $db = null;
-        echo '{"wine": ' . json_encode($wines) . '}';
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
- 
-function getConnection() {
-    $dbhost="127.0.0.1";
-    $dbuser="root";
-    $dbpass="";
-    $dbname="cellar";
-    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $dbh;
 }
  
 ?>
